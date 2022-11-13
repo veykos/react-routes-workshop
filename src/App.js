7795d0ea736781749fe7a9fd7c372e68ca43dc76
Pavel Popov <pcveykos@gmail.com>
2022-11-13 21:13:26 +0100

    Add step 9 of workshop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Dogs from './pages/Dogs';
+import { DogsBreedsPage } from './pages/Dogs/dogs-breed';
 import Cats from './pages/Cats';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Home/>} />
             <Route path="/cats" element={<Cats/>} />
             <Route path="/dogs" element={<Dogs/>} />
+            <Route path="/dogs/:id" element={<DogsBreedsPage />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/Dogs/dogs-breed.jsx
@@ -0,0 +1,22 @@
+import useFetch from "../../hooks/use-fetch";
+import { useParams } from "react-router-dom";
+
+export const DogsBreedsPage = () => {
+  const { id } = useParams();
+  const searchBreedUrl = `https://dog.ceo/api/breed/${id}/images`;
+ 
+  const [selectedBreed, selectedBreedErr, selectedBreedLoading] = useFetch(
+    searchBreedUrl
+  );
+ 
+  if (selectedBreedLoading || !selectedBreed.message)
+    return <div> loading </div>;
+ 
+  if (selectedBreedErr) return <div> error </div>;
+ 
+  return (
+    <main>
+      <img src={selectedBreed.message[0]} alt="hello" />
+    </main>
+  );
+ };
