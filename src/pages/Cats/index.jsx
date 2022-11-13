879ebc2b624ecaed6bf38c46566976b7a00d2dfc
Pavel Popov <pcveykos@gmail.com>
2022-11-13 21:43:25 +0100

    Add bonus bonus - select dropdown for cats

--- src/pages/Cats/index.jsx
+++ src/pages/Cats/index.jsx
@@ -1,23 +1,31 @@
 import useFetch from "../../hooks/use-fetch";
-import {Link} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
+import {useState} from 'react'
 export default function Cats() {
   const getCatBreedsUrl = 'https://api.thecatapi.com/v1/breeds'
+  const [selectedCat,setSelectedCat] = useState(null)
   const [breedsList, breedsListErr, breedsListLoading] = useFetch(getCatBreedsUrl);
-
+  const navigate = useNavigate()
   if (breedsListLoading) return <div> loading </div>;
   if (breedsListErr) return <div> error </div>;
   
-  console.log(breedsList)
+  function handleClick(e) {
+    e.preventDefault()
+    navigate(`/cats/${selectedCat}`)
+  }
 
   return (
     <div>
+            <select defaultValue={'Select a cat'} onChange={e => setSelectedCat(e.target.value)} >
+            <option disabled>Select a cat</option>
       {breedsList.map((ele,idx) => {
           return (
-            <Link key={idx} to={'/cats/' + ele.id}>
-              {ele.name} {" "}
-            </Link>
+            <option key={ele.id} value={ele.id}> {ele.name} 
+            </option>
             )
       })}
+            </select>
+            <button onClick={handleClick} >Go to picture</button>
     </div>
   );
 
