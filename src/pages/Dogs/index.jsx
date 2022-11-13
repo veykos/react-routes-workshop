33f2b4d03426cef66e1e4abf5cf455ffa5e30e67
Pavel Popov <pcveykos@gmail.com>
2022-11-13 20:13:03 +0100

    Add Step 8 of the workshop

--- src/pages/Dogs/index.jsx
+++ src/pages/Dogs/index.jsx
@@ -1,8 +1,25 @@
+import { Link } from "react-router-dom";
+import useFetch from "../../hooks/use-fetch";
+
+
 export default function Dogs() {
+  const getBreedsUrl = "https://dog.ceo/api/breeds/list/all";
+  const [breedsList, breedsListErr, breedsListLoading] = useFetch(getBreedsUrl);
+
+  console.log(breedsList)
+  if (breedsListLoading || !breedsList.message) return <div> loading </div>;
+  if (breedsListErr) return <div> error </div>;
+
+  const filteredBreeds = Object.keys(breedsList.message)
   return (
-    <div>
-      Hello From Dogs Page !
-    </div>
+    <main>
+      {filteredBreeds.map((e, i) => (
+       <Link key={i} to={`/dogs/${e}`}>
+         {e} {""}
+       </Link>
+     ))}
+
+    </main>
   );
 
 }
