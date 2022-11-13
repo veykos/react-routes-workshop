6f412a212e5894dcec359dd6b61110cf80d7f098
Pavel Popov <pcveykos@gmail.com>
2022-11-13 20:05:59 +0100

    Add step 7 of workshop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,17 @@ import { NavLink } from "react-router-dom";
 import classes from "./Header.module.css";
 
 export const Header = () => {
+  const handleActiveLink = ({ isActive }) => {
+    return isActive ? { color: "yellow" } : null;
+  };
+ 
   return (
     <header className={classes.header}>
       <i> site logo </i>
       <nav className={classes.nav}>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/dogs">Dogs</NavLink>
-        <NavLink to="/cats">Cats</NavLink>
+        <NavLink style={handleActiveLink} to="/">Home</NavLink>
+        <NavLink style={handleActiveLink} to="/dogs">Dogs</NavLink>
+        <NavLink style={handleActiveLink} to="/cats">Cats</NavLink>
       </nav>
     </header>
   );
